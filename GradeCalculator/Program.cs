97ff57c7e88a54d17a61a1cb393f92b94b6aeb41
Pritika115 <pritikapritika2005@gmail.com>
2025-04-04 23:04:38 -0400

    Add project files.

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+
+namespace GradeCalculator
+{
+    internal class Program
+    {
+
+        public void main(String[] args)
+        {
+            List<int> marks = new List<int>();
+            int totalMarks = 0;
+            double averageMarks = 0.0;
+            string grade = "";
+
+            InputMarks(marks);
+        }
+        
+
+
+        static void InputMarks(List<int> studentMarks)
+        {
+            Console.Write("Enter the number of subjects: ");
+            int numSubjects = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numSubjects; i++)
+            {
+                Console.Write("Enter marks for subject" + i + "(0-100): ");
+                int mark = int.Parse(Console.ReadLine());
+
+
+                if (mark >= 0 && mark <= 100)
+                {
+                    studentMarks.Add(mark);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid mark. Please enter between 0 and 100.");
+
+                }
+            }
+        }
+        
+
+    }
+}
